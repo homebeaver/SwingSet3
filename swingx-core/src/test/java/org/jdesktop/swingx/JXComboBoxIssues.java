6122ec6e3743d7ed93c180d63ba67ec3e3fbd675
homebeaver <homebeaver@outlook.de>
2026-01-17 19:51:53 +0100

    test for issue https://github.com/homebeaver/SwingSet/issues/69

--- swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxIssues.java
+++ swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxIssues.java
@@ -19,6 +19,7 @@
 package org.jdesktop.swingx;
 
 import java.awt.event.ActionEvent;
+import java.util.Comparator;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
@@ -32,7 +33,11 @@ import javax.swing.JComponent;
 import org.jdesktop.swingx.decorator.HighlighterFactory;
 import org.jdesktop.swingx.decorator.PainterHighlighter;
 import org.jdesktop.swingx.painter.ImagePainter;
+import org.jdesktop.swingx.renderer.DefaultListRenderer;
+import org.jdesktop.swingx.renderer.IconValue;
+import org.jdesktop.swingx.renderer.StringValue;
 import org.jdesktop.swingx.test.XTestUtils;
+import org.junit.Test;
 
 /**
  *
@@ -40,7 +45,6 @@ import org.jdesktop.swingx.test.XTestUtils;
  */
 public class JXComboBoxIssues extends InteractiveTestCase {
     
-    @SuppressWarnings("unused")
     private static final Logger LOG = Logger.getLogger(JXComboBoxIssues.class.getName());
     
     private ComboBoxModel<Object> model;
@@ -117,7 +121,122 @@ public class JXComboBoxIssues extends InteractiveTestCase {
         JXFrame frame = showInFrame(panel, "Highlighter - editable");
         addStatusMessage(frame, "editable xcombo appearance looks okay in Win/Nimbus");
     }
+
+// https://github.com/homebeaver/SwingSet/issues/69 
+    // intentionally unsorted
+	private static final String[] petStrings = { "Tyrannosaurus Rex", "Dog", "Bird", "Cat", "Rabbit", "Pig" };
+    // a controbutor stub
+	static class Contributor {
+		public String getLastName() {
+			return "LastName";
+		}
+		public String getFirstName() {
+			return "FirstName";
+		}
+		public String getMerits() {
+			return "Merits"+this.hashCode();
+		}
+		public String toString() {
+			return getLastName() + ", " + getFirstName() + " (" + getMerits() + ")";
+		}
+	}
+	private static String preferredStringRepresentation(Object o) {
+		if (o == null)
+			return "";
+		// to be used in JXComboBox<Object> xcb
+		if (o instanceof Contributor c) {
+			return c.getLastName() + ", " + c.getFirstName() + " (" + c.getMerits() + ")";
+		}
+		// to be used in controller JXComboBox<DisplayInfo<Icon>> iconChooserCombo
+		return o.toString();
+	}
+
+	/**
+	 * test a sorted combo box with items of different type, define a comparator
+	 * https://github.com/homebeaver/SwingSet/issues/69 :
+	 * version 1.7.9 : BUG when adding items in sorted ComboBoxModel throws ArrayIndexOutOfBoundsException
+	 */
+	@Test
+	public void interactiveSortedComboBoxWithAddedElements() {
+		JXComboBox<Object> xcb;
+		// Create a sorted combo box with Object items; define a comparator
+		xcb = new JXComboBox<>(petStrings, true);
+		xcb.setComparator(new Comparator<Object>() {
+			@Override
+			public int compare(Object o1, Object o2) {
+				String s1 = preferredStringRepresentation(o1);
+				String s2 = preferredStringRepresentation(o2);
+				//System.out.println("Comparator compare " + s1 + " with " + s2);
+				return s1.compareToIgnoreCase(s2);
+			}
+		});
+        // add some contributors
+        ComboBoxModel<Contributor> m = //Contributors.getContributorModel();
+        		new DefaultComboBoxModel<Contributor>();
+        ((DefaultComboBoxModel<Contributor>)m).addElement(new Contributor());
+        ((DefaultComboBoxModel<Contributor>)m).addElement(new Contributor());
+        ((DefaultComboBoxModel<Contributor>)m).addElement(new Contributor());
+        System.out.println("Contributor item count = "+m.getSize());
+        assertEquals(3, m.getSize());
+        assertEquals(petStrings.length, xcb.getModel().getSize());
+        for(int c=0; c<3; c++) {
+        	Contributor contributor = m.getElementAt(c);
+        	System.out.println("TEST : add Contributor " + contributor + " model.Class="+xcb.getModel().getClass());
+        	xcb.addItem(contributor);
+        }
+        assertEquals(petStrings.length+3, xcb.getModel().getSize());
+        xcb.removeItem(m.getElementAt(0));
+
+        // determine selected item (-1 indicates no selection, 0 is the default)
+        if(xcb.getModel().getSize()>0) {
+        	xcb.setSelectedItem(petStrings[1]);
+        }
+
+//        xcb.setToolTipText(getBundleString("cb.toolTipText", toolTipText));
+//        xcb.addActionListener(ae -> {
+//            Object o = xcb.getSelectedItem();
+//            if(o instanceof String petName) {
+//                updateLabel(rightpic, petName);
+//            } else {
+//                updateLabel(rightpic, o.toString());
+//            }
+//        });
+        // setRenderer(ListCellRenderer<? super E> renderer)
+        StringValue sv = (Object value) -> {
+        	return preferredStringRepresentation(value);
+        };
+//		IconValue iv = (Object value) -> {
+//			if (value instanceof Contributor c) {
+//				return flagIcons[(c.getMerits()) % flagIcons.length];
+//			}
+//			return IconValue.NULL_ICON;
+//		};
+//        xcb.setRenderer(new DefaultListRenderer<Object>(sv, iv));
+        xcb.setRenderer(new DefaultListRenderer<Object>(sv));
+//        right.add(xcb, BorderLayout.CENTER);
+//        
+//        //Set up the item picture.
+//        rightpic = new JLabel();
+//        rightpic.setFont(rightpic.getFont().deriveFont(Font.ITALIC));
+//        rightpic.setHorizontalAlignment(JLabel.CENTER);
+//        int si = xcb.getSelectedIndex();
+//        if (si<0 || si>=petStrings.length) {
+//        	updateLabel(rightpic, "unbekannt");
+//        } else {
+//        	updateLabel(rightpic, petStrings[si]);
+//        }
+//        rightpic.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+//        right.add(rightpic, BorderLayout.PAGE_END);
+//        
+//        right.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+//        return right;
+
+        JComponent panel = new JXPanel();
+        panel.add(xcb);
+        showInFrame(panel, "SortedComboBoxWithAddedElements");
+	}
     
-    
-    public void testDummy() { }
+    public void testDummy() {
+    	interactiveSortedComboBoxWithAddedElements();
+    }
 }
